8007cc4b45fce4cde29f70f21f6467f48428fa2f
Birk Nergaard <birknergard@gmail.com>
2025-11-10 10:46:57 +0100

    .

--- components/create/form/sections.tsx
+++ components/create/form/sections.tsx
@@ -1,6 +1,41 @@
 import { View } from "react-native";
 
-export function TitleAndDescription({ }: {}) {
+export function TitleAndDescriptionSelection({ }: {}) {
+  return (
+    <View className={s.main}>
+    </View>
+  );
+}
+
+export function CategorySelection({ }: {}) {
+  return (
+    <View className={s.main}>
+    </View>
+  );
+}
+
+export function DateAndTimeSelection({ }: {}) {
+  return (
+    <View className={s.main}>
+    </View>
+  );
+}
+
+export function PlaceSelection({ }: {}) {
+  return (
+    <View className={s.main}>
+    </View>
+  );
+}
+
+export function PersonsSelection({ }: {}) {
+  return (
+    <View className={s.main}>
+    </View>
+  );
+}
+
+export function ImageUpload({ }: {}) {
   return (
     <View className={s.main}>
     </View>
