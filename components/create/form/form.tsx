4a0202895988b985b92b3b28b25e69791bddeb79
Birk Nergaard <birknergard@gmail.com>
2025-11-10 13:21:50 +0100

    feat: created initial state fields for form, created simple textboxes for title and descripiton step.

--- components/create/form/form.tsx
+++ components/create/form/form.tsx
@@ -1,19 +1,52 @@
 import { createConstants } from 'constants/create';
 import { Text, View } from 'react-native';
 import styled from 'styled-components/native';
+import {
+  CategorySelection,
+  DateAndTimeSelection,
+  ImageUpload,
+  PersonsSelection,
+  PlaceSelection,
+  TitleAndDescriptionSelection,
+} from './sections';
+import { useState } from 'react';
 
 export default function FormSection({ step }: { step: number }) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [address, setAddress] = useState('');
+  const [postcode, setPostcode] = useState('');
+  const [date, setDate] = useState<Date | undefined>();
+  const [timeFrom, setTimeFrom] = useState<Date | undefined>();
+  const [timeTo, setTimeTo] = useState<Date | undefined>();
+  //const images = useState('')
+
+  const SectionList = [
+    <CategorySelection />,
+    <TitleAndDescriptionSelection
+      title={title}
+      onChangeTitle={setTitle}
+      description={description}
+      onChangeDescription={setDescription}
+    />,
+    <PlaceSelection />,
+    <DateAndTimeSelection />,
+    <PersonsSelection />,
+    <ImageUpload />,
+  ];
 
   return (
     <Main>
-      {createConstants.sections.map((section, i) => (
-        step === i + 1 && (
-          <>
-            <Text className={s.section.title}>{section.title}</Text>
-            <Text className={s.section.description}>{section.description}</Text>
-          </>
-        )
-      ))}
+      {createConstants.sections.map(
+        (section, i) =>
+          step === i + 1 && (
+            <View className={s.section.base}>
+              <Text className={s.section.title}>{section.title}</Text>
+              {/* <Text className={s.section.description}>{section.description}</Text> */}
+              {SectionList[i]}
+            </View>
+          )
+      )}
     </Main>
   );
 }
@@ -27,7 +60,8 @@ const Main = styled.View`
 
 const s = {
   section: {
+    base: 'w-full flex flex-col items-center justify-center gap-4',
     title: 'text-3xl text-dugnad-red font-bold',
-    description: 'text-lg'
-  }
-}
+    description: 'text-lg',
+  },
+};
--- components/create/form/sections.tsx
+++ components/create/form/sections.tsx
@@ -1,47 +1,89 @@
-import { View } from "react-native";
+import { Text, TextInput, View } from 'react-native';
 
-export function TitleAndDescriptionSelection({ }: {}) {
+export function CategorySelection({}: {}) {
   return (
     <View className={s.main}>
+      <Text>Category</Text>
     </View>
   );
 }
 
-export function CategorySelection({ }: {}) {
+export function TitleAndDescriptionSelection({
+  title,
+  onChangeTitle,
+  description,
+  onChangeDescription,
+}: {
+  title: string;
+  onChangeTitle: (e: string) => void;
+  description: string;
+  onChangeDescription: (e: string) => void;
+}) {
   return (
     <View className={s.main}>
+      <View className={s.fieldContainer.base}>
+        <Text className={s.fieldContainer.desc}>
+          1. Please create a fitting title for your dugnad
+        </Text>
+        <TextInput
+          className={s.fieldContainer.textField}
+          onChangeText={(e) => onChangeTitle(e)}
+          value={title}
+          placeholder="Your new title ..."
+        />
+      </View>
+      <View className={s.fieldContainer.base}>
+        <Text className={s.fieldContainer.desc}>2. Please describe your dugnad.</Text>
+        <TextInput
+          className={s.fieldContainer.textField}
+          onChangeText={(e) => onChangeDescription(e)}
+          value={description}
+          multiline={true}
+          numberOfLines={4}
+          placeholder="Write a description ..."
+        />
+      </View>
     </View>
   );
 }
 
-export function DateAndTimeSelection({ }: {}) {
+export function DateAndTimeSelection({}: {}) {
   return (
     <View className={s.main}>
+      <Text>Date and time</Text>
     </View>
   );
 }
 
-export function PlaceSelection({ }: {}) {
+export function PlaceSelection({}: {}) {
   return (
     <View className={s.main}>
+      <Text>Place</Text>
     </View>
   );
 }
 
-export function PersonsSelection({ }: {}) {
+export function PersonsSelection({}: {}) {
   return (
     <View className={s.main}>
+      <Text>Persons</Text>
     </View>
   );
 }
 
-export function ImageUpload({ }: {}) {
+export function ImageUpload({}: {}) {
   return (
     <View className={s.main}>
+      <Text>Images</Text>
     </View>
   );
 }
 
 const s = {
-  main: ''
-}
+  main: 'w-full h-full flex flex-col items-center justify-evenly bg-dugnad-red rounded-xl p-3',
+  fieldContainer: {
+    base: 'w-full flex flex-col items-center',
+    desc: 'text-lg text-white text-start',
+    textField: 'w-3/4 text-lg rounded-md bg-dugnad-white p-2',
+  },
+};
