ea9fd8e0a0e9952e379ca26e3dd9193b722e6555
Birk Nergaard <birknergard@gmail.com>
2025-11-12 12:36:42 +0100

    feat: can now register new users from login page

--- app/login.tsx
+++ app/login.tsx
@@ -1,28 +1,27 @@
 import { useState } from "react";
-import { auth } from "firebaseConfig";
 import { Pressable, Text, TextInput, View } from "react-native";
-import * as AuthSession from 'expo-auth-session';
 import * as WebBrowser from "expo-web-browser";
-import * as AuthActions from 'services/authService'
-import { makeRedirectUri } from "expo-auth-session";
-import { signIn } from "services/authService";
-import { reload } from "firebase/auth";
+import { signUp } from 'services/authService';
 import { useAuthSession } from "providers/authSessionProvider";
+import TextButton from "components/general/textButton";
 WebBrowser.maybeCompleteAuthSession();
 
 export default function Login() {
 
-  const { signIn, signOut } = useAuthSession()
+  const { signIn, signOut, user } = useAuthSession()
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [verifiedPassword, setVerifiedPassword] = useState('');
   const [email, setEmail] = useState('');
   const [isRegister, setIsRegister] = useState(false);
 
   const tryLogin = async () => {
     try {
       if (isRegister) {
-        await AuthActions.signUp(email, password, username);
+        if (password === verifiedPassword) {
+          await signUp(email, password, username);
+        }
       } else {
         signIn(email, password);
       }
@@ -31,44 +30,103 @@ export default function Login() {
     }
   }
 
-  const tryLogout = async () => {
-    try {
-      signOut();
-    } catch (error: any) {
-      console.error('Logout failed')
-    }
-  }
-
   return (
-    <View className='flex flex-col'>
-      <Text>Email</Text>
-      <TextInput
-        value={email}
-        onChangeText={setEmail}
-      />
-      <Text>Password</Text>
-      <TextInput
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        textContentType="password"
-      />
-      <View>
-        <Pressable
-          onPress={async () => {
-            await tryLogin()
-          }}
-        >
+    <View className={s.mainContainer}>
+      <View className={s.loginContainer}>
+        <Text className={s.title}>DugnadHub</Text>
+        {!isRegister ? (<>
           <Text>Login</Text>
-        </Pressable>
-        <Pressable
-          onPress={async () => {
-            await tryLogout()
-          }}
-        >
-          <Text>Logout</Text>
-        </Pressable>
+          <View className={s.fieldContainer}>
+            <Text className={s.label}>Email</Text>
+            <TextInput className={s.input}
+              value={email}
+              onChangeText={setEmail}
+            />
+          </View>
+          <View className={s.fieldContainer}>
+            <Text className={s.label}>Password</Text>
+            <TextInput className={s.input}
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+              textContentType="password"
+            />
+          </View>
+          <View className={s.buttonContainer}>
+            <TextButton
+              text='Login'
+              iconName=''
+              iconPosition=''
+              onTap={async () => {
+                await tryLogin()
+              }}
+            />
+            <TextButton
+              text='Register'
+              iconName=''
+              iconPosition=''
+              onTap={() => setIsRegister(true)}
+            />
+          </View>
+        </>) : (<>
+          <Text>Register new user</Text>
+          <View className={s.fieldContainer}>
+            <Text className={s.label}>Create username</Text>
+            <TextInput className={s.input}
+              value={username}
+              onChangeText={setUsername}
+            />
+          </View>
+          <View className={s.fieldContainer}>
+            <Text className={s.label}>Email</Text>
+            <TextInput className={s.input}
+              value={email}
+              onChangeText={setEmail}
+            />
+          </View>
+          <View className={s.fieldContainer}>
+            <Text className={s.label}>Create password</Text>
+            <TextInput className={s.input}
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+              textContentType="password"
+            />
+          </View>
+
+          <View className={s.fieldContainer}>
+            <Text className={s.label}>Verify password</Text>
+            <TextInput className={s.input}
+              value={verifiedPassword}
+              onChangeText={setVerifiedPassword}
+              secureTextEntry
+              textContentType="password"
+            />
+          </View>
+          <View className={s.buttonContainer}>
+            <TextButton
+              text='Register'
+              iconName=''
+              iconPosition=''
+              onTap={async () => {
+                await tryLogin();
+              }}
+            />
+          </View>
+        </>)}
       </View>
     </View>
   );
+}
+
+const s = {
+  mainContainer: 'flex flex-1 flex-col items-center justify-center bg-dugnad-white',
+  title: 'text-3xl',
+  loginContainer: 'flex flex-col items-center justify-between bg-dugnad-red rounded-3xl p-8 gap-4',
+  fieldContainer: '',
+  label: 'text-xl',
+  buttonContainer: 'flex flex-col items-center justify-center gap-2',
+  button: 'bg-dugnad-yellow p-4',
+  buttonText: 'text-xl',
+  input: 'bg-white text-lg rounded-lg p-2',
 }
