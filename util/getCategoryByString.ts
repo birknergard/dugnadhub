6a6da0bb1da65e81ade15d55942f698594ffce8f
Birk Nergaard <birknergard@gmail.com>
2025-11-18 18:44:49 +0100

    chore: removed extraneous util (moved to category definition)

--- util/getCategoryByString.ts
+++
@@ -1,7 +0,0 @@
-import { Category, categoryConstants } from "constants/createConstants";
-
-export default function getCategoryByName(name: string): Category | null {
-  return categoryConstants.find(category => {
-    return category.name === name
-  }) ?? null;
-}
