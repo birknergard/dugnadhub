b6eea8b22c4dc2fb49a379ee29d2a1cd5e2c1d8b
Birk Kristinius Nergaard <61409890+birknergard@users.noreply.github.com>
2025-12-04 13:14:40 +0100

    Delete firebaseEnv.js

--- firebaseEnv.js
+++
@@ -1,9 +0,0 @@
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyCMNUgBFliIS3La1KFsYvI3txmd3au8PzU",
-  authDomain: "cross-df1fa.firebaseapp.com",
-  projectId: "cross-df1fa",
-  storageBucket: "cross-df1fa.firebasestorage.app",
-  messagingSenderId: "980671944241",
-  appId: "1:980671944241:web:3d04620ab9454682406710"
-};
